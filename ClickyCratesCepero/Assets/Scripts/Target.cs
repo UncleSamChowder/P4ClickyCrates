fa292f8786ee291c70f1e4dee149aeff0df0ded7
UncleSamChowder <745603@student.dusd.net>
2025-05-20 11:43:53 -0700

    gert4re
    t4ertretrtr

--- ClickyCratesCepero/Assets/Scripts/Target.cs
+++ ClickyCratesCepero/Assets/Scripts/Target.cs
@@ -9,20 +9,35 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float yRange = -6;
+    private float ySpawnPos = -6;
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
-        targetRb.AddForce(Vector3.up * Random.Range(minSpeed), maxSpeed),ForceMode.Impulse);
-        targetRb.AddTorque(Random.Range(-maxTorque,maxTorque),Random.Range(-maxTorque,maxTorque),
-            Random.Range(10,10), ForceMode.Impulse);
-        transform.position = new Vector3(Random.Range(-4, 4), -6);
+
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque() , RandomTorque(),
+            RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 }
